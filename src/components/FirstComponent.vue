180f57dcc3ff692622e8ea5c661cce99d25b3f5e
muhammad-awan <muhammad.adam.awan@gmail.com>
2018-04-11 21:28:56 +1000

    Vue app boilerplate with CLI

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="FirstComponent">
+    {{myData}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FirstComponent',
+  data () {
+    return {
+      myData: 20
+    }
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="HelloWorld">
+    {{msg}}
+    <first-component></first-component>
+  </div>
+</template>
+
+<script>
+import FirstComponent from './FirstComponent'
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    FirstComponent
+  },
+  data () {
+    return {
+      msg: 'Hello World'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
